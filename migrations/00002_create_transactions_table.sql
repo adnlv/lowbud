28ca2722ffcc0cc2da66956c21f9207c4bab56a4
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-08-14 22:03:24 +0300

    Add amount field to transactions table

--- migrations/00002_create_transactions_table.sql
+++ migrations/00002_create_transactions_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE transactions (
     source_account UUID NOT NULL REFERENCES accounts (id),
     destination_account UUID NOT NULL REFERENCES accounts (id),
     currency_code VARCHAR(3) NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
