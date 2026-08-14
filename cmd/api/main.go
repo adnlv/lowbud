cde7df4ada9850f7d97cfd2b4898a196fb442547
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-08-14 20:32:53 +0300

    Init logger and set up HTTP server with graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,51 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:         net.JoinHostPort("localhost", "8080"),
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Listen and serve: %s", err)
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	slog.Debug("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server shutdown: %s", err)
+		os.Exit(1)
+	}
+	slog.Debug("Server shut down")
+}
