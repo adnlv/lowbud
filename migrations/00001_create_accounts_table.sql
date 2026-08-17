d45c2e45a9ee638a522a4c8b445fbebb3686d768
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-08-17 16:33:14 +0300

    Apply conditional unique index on accounts email

--- migrations/00001_create_accounts_table.sql
+++ migrations/00001_create_accounts_table.sql
@@ -1,14 +1,18 @@
 -- +goose Up
 CREATE TABLE accounts (
     id UUID PRIMARY KEY,
+    email TEXT NOT NULL,
     forename TEXT NOT NULL,
     surname TEXT NOT NULL,
-    email TEXT NOT NULL,
     password_hash TEXT NOT NULL,
     registered_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     closed_at TIMESTAMPTZ
 );
+
+CREATE UNIQUE INDEX idx_accounts_active_email
+ON accounts (email)
+WHERE closed_at IS NULL;
 
 -- +goose Down
 DROP TABLE accounts;
